b284e689ec5c2a442a1f879bf2c354672fc1c24b
S. Ryan <sokuenryan@gmail.com>
2024-07-18 20:28:29 -0400

    got rid of all the extra code that was unused in the helpers and logout files.

--- src/helpers.js
+++ src/helpers.js
@@ -1,11 +1,14 @@
-export const waait = () => new Promise(res => 
-    setTimeout(res, Math.random() *800))
-
+// local storage
 export const fetchData = (key) => {
     return JSON.parse(localStorage.getItem(key));
 };
 
-export const getAllMatchingItems = ({category, key, value}) => {
-    const data = fetchData(category) ?? [];
-    return data.filter((item) => item[key] === value)
-};
+// delete item from local storage
+export const deleteItem = ({key, id}) =>  {
+    const existingData = fetchData(key);
+    if(id) {
+        const newData = existingData.filter((item) => item.id !== id)
+        return localStorage.setItem(key, JSON.stringify(newData));
+    }
+    return localStorage.removeItem(key);
+}
