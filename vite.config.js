e2ff0982f95a6a381c29e512c086abb074e897a1
S. Ryan <sokuenryan@gmail.com>
2024-08-23 23:07:51 -0400

    changes made to the vite.config.js file

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist' // Specify the output directory here
+    outDir: 'dist',
+  },
+  base: '/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@/components': path.resolve(__dirname, 'src/components'), 
+      '@/helpers': path.resolve(__dirname, 'src/helpers'), 
+    }
   }
 });
